dde63f856b66a5008deeaad47dc87a11c9a03a1e
absunstar <absunstar@gmail.com>
2025-12-31 18:03:38 +0200

    #

--- browser_files/overwrite/js/recaptcha.js
+++ browser_files/overwrite/js/recaptcha.js
@@ -2,36 +2,34 @@
     var onload = '/*query.onload*/';
 
     var grecaptcha = {};
-    grecaptcha.render = function(id , options){
+    grecaptcha.render = function (id, options) {
         let div = document.querySelector('#' + id);
-        if(div){
-            div.innerHTML = "recaptcha hacked HTML";
+        if (div) {
+            div.innerHTML = 'recaptcha hacked HTML';
         }
-        if(options && options.callback){
+        if (options && options.callback) {
             w[callback]('succeed');
         }
-        if(onload && w[onload]){
+        if (onload && w[onload]) {
             w[onload]('succeed');
         }
     };
-  
-    grecaptcha.getResponse = function(){
-        return 'succeed';
-    }
 
+    grecaptcha.getResponse = function () {
+        return 'succeed';
+    };
 
     w.grecaptcha = grecaptcha;
     setInterval(() => {
         let el = document.querySelector('.g-recaptcha');
         if (el) {
-
-            if(!document.querySelector('.g-recaptcha-response')){
+            if (!document.querySelector('.g-recaptcha-response')) {
                 let ta = document.createElement('textarea');
-                ta.id="g-recaptcha-response";
-                ta.name="g-recaptcha-response";
-                ta.className = "g-recaptcha-response";
-                ta.style.display = "none";
-                ta.value = "xxxxxxxxxxxxxx";
+                ta.id = 'g-recaptcha-response';
+                ta.name = 'g-recaptcha-response';
+                ta.className = 'g-recaptcha-response';
+                ta.style.display = 'none';
+                ta.value = 'xxxxxxxxxxxxxx';
                 el.parentNode.appendChild(ta);
             }
 
